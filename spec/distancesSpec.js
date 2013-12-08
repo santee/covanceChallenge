95f703eeff826dc8eb4774529f8afc53fa58f999
Alexander Shevnin <rheisthlin@gmail.com>
2013-12-09 02:33:14 +0400

    Euclidean distance fix

--- spec/distancesSpec.js
+++ spec/distancesSpec.js
@@ -2,7 +2,7 @@
 
 var distances = require('../lib/distances.js');
 
-describe('Distances: euclidean', function () {
+describe('Distances: euclidean distance', function () {
 
   it('should calculate distance between simple objects', function () {
 
@@ -12,4 +12,16 @@ describe('Distances: euclidean', function () {
     var distance = distances.euclideanDistance(a, b);
     expect(distance).toBe(2);
   });
+
+
+  it('should work if string properties are presented in object', function () {
+
+    var a = { x : 0, y : 3, text: 'test' };
+    var b = { x : 0, y : 1, text2: 'test3' };
+
+    var distance = distances.euclideanDistance(a, b);
+    expect(distance).toBe(2);
+  });
+
+
 });
