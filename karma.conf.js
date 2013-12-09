abaa69498cf249d1d0e564a79db35154cd3e5fcf
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2013-12-09 15:30:13 +0400

    Iris data set, doubles are doubles now

--- karma.conf.js
+++ karma.conf.js
@@ -48,7 +48,7 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome','IE','Firefox'],
+    browsers: ['Chrome','IE'],
 
 
     // Continuous Integration mode
