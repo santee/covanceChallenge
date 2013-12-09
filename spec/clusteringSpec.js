b7c601214546e492ebb979d35c350f1297a12fcc
Alexander Shevnin <alexander.shevnin@arcadia.spb.ru>
2013-12-09 17:08:57 +0400

    Hierarhical clustering implemented. Small data used to avoid recursion issues

---
+++ spec/clusteringSpec.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var distances = require('../lib/distances.js');
+var clusterizer = require('../lib/clusterizer.js');
+var iris = require('../lib/irisFlowerDataSet.js');
+
+describe('Clusters: k-means', function(){
+  it('should contain same amount of elements after clustering', function() {
+    var clusters = clusterizer.kMeans(iris.data, distances.euclideanDistance, distances.averageCoordinatesFinder, 2);
+
+    expect(clusters.length).toBe(2);
+    expect(clusters[0].length + clusters[1].length).toBe(iris.data.length);
+  });
+});
+
+describe('Clusters: hierarhical', function() {
+  it('should contain same amount of elements after clustering', function() {
+
+    //var data = iris.data;
+
+    var data = iris.data.slice(0, 50);
+
+    var clusters = clusterizer.hieararhicalKMeans(data, distances.euclideanDistance, distances.averageCoordinatesFinder);
+
+    expect(clusters.size()).toBe(data.length);
+  });
+});
