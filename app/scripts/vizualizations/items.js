f5b0d701fd6cfbe0f271f9097e5fb0d4d4a4eff1
Alexander Shevnin <rheisthlin@gmail.com>
2013-12-13 11:57:40 +0400

    Items controller fixed

--- app/scripts/vizualizations/items.js
+++ app/scripts/vizualizations/items.js
@@ -1,12 +1,16 @@
 'use strict';
 
 angular.module('items', ['dataProvider'])
-  .controller('clustersListCtrl', ['$scope', 'clusteredData', function ($scope, clusteredData) {
+  .controller('clustersListCtrl', ['$scope', 'clusteredData', 'itemsSelectionService', function ($scope, clusteredData, itemsSelectionService) {
 
     $scope.allItems = [];
     $scope.cluster = {};
     $scope.commonTextProperties = [];
     $scope.commonNumericProperties = [];
+    $scope.itemClick = function (item) {
+      itemsSelectionService.currentSelector = itemsSelectionService.Selectors.ITEMS;
+      item.toggleSelect();
+    };
 
     clusteredData.then(function(cluster) {
         $scope.allItems = cluster.getAllItems();
