290f0e40315aaca13275bca1cbc2385e623dac47
TIGERoX <tigerox@mail.ru>
2013-12-17 22:46:07 +0400

    Added missed files;

---
+++ app/scripts/controllers/navigation.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('covanceChallengeApp')
+  .controller('NavigationCtrl', ['$scope', '$location', '$log', 'datasetList', function ($scope, $location, $log, datasetList) {
+
+    $scope.maxDepth = 5;
+    $scope.datasets = [];
+
+    $scope.dataset = $location.search()['setName'];
+    $log.info('dataset ' + $scope.dataset);
+
+    datasetList.then(function (datasets) {
+      $scope.datasets = datasets.data;
+      if (!$scope.dataset) {
+        $scope.dataset = $scope.datasets[0];
+        $location.search('setName', $scope.dataset);
+      }
+    });
+
+  }]);
---
+++ app/scripts/dataProvider/datasetList.js
@@ -0,0 +1,6 @@
+﻿'use strict';
+
+angular.module('dataProvider')
+  .factory('datasetList', ['$q', '$http', function ($q, $http) {
+    return $http.get('/api/datasetnames');
+  }]);
