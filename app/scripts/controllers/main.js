49eccda907f1765ebe76eeed811c2aa282f4c5fe
TIGERoX <tigerox@mail.ru>
2013-12-18 14:05:59 +0400

    Added busy indicator;

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,12 +5,13 @@ angular.module('covanceChallengeApp')
 
     $scope.displayDepth = 0;
     $scope.maxDepth = 0;
-
+    $scope.isBusy = true;
 
     clusteredData.then(function (clusters) {
       $scope.clusters = clusters;
       $scope.maxDepth = clusters.findMaxDepth();
       $scope.displayDepth = Math.min(5, $scope.maxDepth);
+      $scope.isBusy= false;      
     });
 
     $scope.$watch('displayDepth', function (newValue) {
